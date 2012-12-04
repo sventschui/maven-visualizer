ef50016fff0bc77aafb2520d81aec63b7045b902
Sven Tschui <tschui@webmate.ch>
2012-12-04 21:28:41 +0100

    Exception handling and logging

--- src/main/java/ch/sventschui/maven/visualizer/VisualizerMojo.java
+++ src/main/java/ch/sventschui/maven/visualizer/VisualizerMojo.java
@@ -29,6 +29,8 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ch.sventschui.maven.visualizer.model.MavenArtifact;
 import ch.sventschui.maven.visualizer.model.MavenArtifactStore;
@@ -41,111 +43,117 @@ import ch.sventschui.maven.visualizer.model.MavenArtifactStore;
  * @phase process-sources
  */
 public class VisualizerMojo extends AbstractMojo {
-    /**
-     * Location of the file.
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    private File projectBuildDir;
+	private static Logger logger = LoggerFactory
+			.getLogger(VisualizerMojo.class);
 
-    private File outputDir = null;
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${project.build.directory}"
+	 * @required
+	 */
+	private File projectBuildDir;
 
-    private List<File> poms = new Vector<File>();
+	private File outputDir = null;
 
-    private MavenArtifactStore store = new MavenArtifactStore();
+	private List<File> poms = new Vector<File>();
 
-    /**
-     * @parameter
-     * @required
-     */
-    private String[] directories;
+	private MavenArtifactStore store = new MavenArtifactStore();
 
-    /**
-     * @parameter
-     * @required
-     */
-    private String[] includes;
+	/**
+	 * @parameter
+	 * @required
+	 */
+	private String[] directories;
 
-    public void execute() throws MojoExecutionException {
+	/**
+	 * @parameter
+	 * @required
+	 */
+	private String[] includes;
 
-        createOutputDir();
+	public void execute() throws MojoExecutionException {
 
-        for (String directory : this.directories) {
-            poms.addAll(FileUtils.listFiles(new File(directory), new PomFileFilter(), new TargetFileFilter()));
-        }
+		this.createOutputDir();
 
-        for (File pom : poms) {
+		for (String directory : this.directories) {
+			this.poms.addAll(FileUtils.listFiles(new File(directory),
+					new PomFileFilter(), new TargetFileFilter()));
+		}
 
-            Model model = null;
+		for (File pom : this.poms) {
 
-            FileReader reader = null;
+			Model model = null;
 
-            MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+			FileReader reader = null;
 
-            try {
-                reader = new FileReader(pom);
-                model = mavenreader.read(reader);
-                model.setPomFile(pom);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (XmlPullParserException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+			MavenXpp3Reader mavenreader = new MavenXpp3Reader();
 
-            MavenProject project = new MavenProject(model);
+			try {
+				reader = new FileReader(pom);
+				model = mavenreader.read(reader);
+				model.setPomFile(pom);
+			} catch (IOException e) {
+				logger.error("Failed to read pom " + pom.getAbsolutePath(), e);
+			} catch (XmlPullParserException e) {
+				logger.error("Failed to parse pom " + pom.getAbsolutePath(), e);
+			}
 
-            if (this.matchesFilter(project.getGroupId())) {
+			MavenProject project = new MavenProject(model);
 
-                MavenArtifact base = new MavenArtifact(project.getGroupId(), project.getArtifactId(),
-                        project.getVersion());
+			if (this.matchesFilter(project.getGroupId())) {
 
-                store.addArtifact(base);
+				MavenArtifact base = new MavenArtifact(project.getGroupId(),
+						project.getArtifactId(), project.getVersion());
 
-                for (Dependency artifact : project.getDependencies()) {
-                    if (matchesFilter(artifact.getGroupId())) {
-                        MavenArtifact dependency = new MavenArtifact(artifact.getGroupId(), artifact.getArtifactId(),
-                                artifact.getVersion());
+				this.store.addArtifact(base);
 
-                        store.addArtifact(dependency);
-                        store.addRelationship(base, dependency);
-                    }
-                }
-            }
-        }
+				for (Dependency artifact : project.getDependencies()) {
+					if (this.matchesFilter(artifact.getGroupId())) {
+						MavenArtifact dependency = new MavenArtifact(
+								artifact.getGroupId(),
+								artifact.getArtifactId(), artifact.getVersion());
 
-        try {
-            FileWriter fstream = new FileWriter(new File(outputDir, "test.json"));
-            BufferedWriter out = new BufferedWriter(fstream);
+						this.store.addArtifact(dependency);
+						this.store.addRelationship(base, dependency);
+					}
+				}
+			}
+		}
 
-            out.write(store.toJSON());
+		try {
+			FileWriter fstream = new FileWriter(new File(this.outputDir,
+					"test.json"));
+			BufferedWriter out = new BufferedWriter(fstream);
 
-            out.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+			out.write(this.store.toJSON());
 
-    }
+			out.close();
+		} catch (IOException e) {
+			logger.error("Failed to write result", e);
+		}
 
-    private void createOutputDir() {
-        this.outputDir = new File(projectBuildDir, "visualizer");
+	}
 
-        // clean
-        if (this.outputDir.exists()) {
-            this.outputDir.delete();
-        }
+	private void createOutputDir() {
+		this.outputDir = new File(this.projectBuildDir, "visualizer");
 
-        // create
-        this.outputDir.mkdirs();
-    }
+		// clean
+		if (this.outputDir.exists()) {
+			this.outputDir.delete();
+		}
 
-    public boolean matchesFilter(String groupId) {
-        for (String include : includes) {
-            if (groupId.startsWith(include)) return true;
-        }
+		// create
+		this.outputDir.mkdirs();
+	}
 
-        return false;
-    }
+	public boolean matchesFilter(String groupId) {
+		for (String include : this.includes) {
+			if (groupId.startsWith(include)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
